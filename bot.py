12443df464984e2b135d7ec60bda0ffcb7a7370b
VİOS HOLDİNG LTD. <erollitex@gmail.com>
2025-06-25 02:30:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ START = f"""
 Süpriz Mail Kutusuna Hediye bırakabilirim Ama Noel Baba Değilim 
 """
 s = bytearray.fromhex("68 74 74 70 73 3A 2F 2F 67 69 74 68 75 62 2E 63 6F 6D 2F 4E 6F 6F 62 2D 6D 75 6B 65 73 68 2F 43 68 61 74 67 70 74 2D 62 6F 74").decode()
-SOURCE = xa
+SOURCE = s
 SOURCE_TEXT = f"""
 ๏ Heyyo,
 """
