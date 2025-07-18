832445d5cb865dc42f3c276c56f4d13eefc37aa3
VİOS HOLDİNG LTD. <erollitex@gmail.com>
2025-07-18 23:41:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ import os
 API_ID = int(os.environ.get("API_ID", "20658336"))
 API_HASH = os.environ.get("API_HASH", "cedfb5fb4ffee7ecc746b28afc7925e3") 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
- 
+
+# TWİLİO SMS APİ
+TWILIO_SID = "AC1efa1740d412dfd8a12328bde295c88e"  # Örnek SID
+TWILIO_TOKEN = ""  # Örnek Token
+TWILIO_SERVICE_SID = " "  # Messaging Service SID
+
 BOT_USERNAME = os.environ.get("BOT_USERNAME" , "HostRioBot") 
 SUDO=os.environ.get("SUDO","5910057231")
 BOT_NAME = os.environ.get("BOT_NAME", "Send Bot")
